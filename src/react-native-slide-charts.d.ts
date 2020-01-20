7f7c08c18664cee4950c872cee5f3070d342e62c
Neil Hannah <ne.hannah@gmail.com>
2020-01-20 02:41:30 +0100

    update types

--- src/react-native-slide-charts.d.ts
+++ src/react-native-slide-charts.d.ts
@@ -5,10 +5,14 @@ import {
   XAxisMarkerProps,
   XAxisLabelAlignment,
   YAxisLabelAlignment,
+  LabelAndAlignment,
 } from './lib/components/chartComponents/axis/utils/types'
 import { CursorProps } from './lib/components/cursor/utils/types'
 import { ToolTipProps, ToolTipTextRenderersInput } from './lib/components/toolTip/utils/types'
 import { GradientProps } from './lib/components/chartComponents/charts/utils/types'
+
+export const SlideBarChart: React.ComponentClass<SlideBarChartProps>
+export const SlideAreaChart: React.ComponentClass<SlideAreaChartProps>
 
 export {
   SlideBarChartProps,
